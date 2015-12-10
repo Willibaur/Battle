84f26358c799649d898dd60666ae095eed7af1ad
willibaur <williambautista@gmail.com>
2015-12-10 05:56:03 +0000

    Step 23 refactored bugs fixed, now all tests passed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,8 +3,9 @@ require 'player'
 describe Player do
   subject(:david) {Player.new('David')}
 
-  describe '#name'
-  it 'Displays class name' do
-    expect(player.name).to eq 'David'
+  describe '#name' do
+    it 'retunrs name' do
+      expect(david.name).to eq 'David'
+    end
   end
 end
