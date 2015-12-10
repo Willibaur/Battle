e47fab64942c84daf06536b9d699f309e7ac2142
William Bautista <Willibaur@users.noreply.github.com>
2015-12-10 13:41:46 +0000

    Step 26

--- app.rb
+++ app.rb
@@ -14,24 +14,23 @@ class Battle < Sinatra::Base
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
+    $game = Game.new($player_1, $player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    $game = Game.new(@player_1, @player_2)
+    @game = $game
     erb :play
   end
 
-
+#vxv
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    $game.attack(@player_2)
+    @game = $game
+    $game.attack(@game.player2)
     erb :attack
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
